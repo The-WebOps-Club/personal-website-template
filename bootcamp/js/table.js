b0ce5390103f944f3194d12dbf7c099973c93e36
Ritviksiddha Penchala <ritviksiddha@gmail.com>
2022-01-03 08:52:19 -0800

    Update table.js
    Removed unnecessary code

--- bootcamp/js/table.js
+++ bootcamp/js/table.js
@@ -1,16 +1,3 @@
-function loadData() {
-    var url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTvv7k_2IQ5OjN2q4ihFmv68rk9SotIgTcvP1zAOgvMACC2jBpsnRknn6ZkQnVHHuoDT2eKVYQBAGqa/pub?gid=0&single=true&output=csv";
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4) {
-            document.getElementById("display").innerHTML = xmlhttp.responseText;
-            //alert(xmlhttp.responseText);
-        }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send(null);
-}
-
 function dataAlert() {
     $.ajax("https://docs.google.com/spreadsheets/d/e/2PACX-1vTvv7k_2IQ5OjN2q4ihFmv68rk9SotIgTcvP1zAOgvMACC2jBpsnRknn6ZkQnVHHuoDT2eKVYQBAGqa/pub?gid=0&single=true&output=csv").done(function(result){
         var datapoints = result.split(/\n/).join(",").split(",");
