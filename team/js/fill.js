59229273844cf2ba1db4fe4ac71a1012430ae8e9
Ishaan Gupta <60420820+IshaanSD@users.noreply.github.com>
2022-01-14 13:32:20 -0800

    imag link

--- team/js/fill.js
+++ team/js/fill.js
@@ -3,9 +3,10 @@ function dataAlert() {
         var datapoints = result.split(/\n/).join("\t").split("\t");
         for(let i = 6; i<datapoints.length; i+=6){
             // var input = '<tr><th scope="row">' + datapoints[i] + '</th><td><a href="#">' +datapoints[i+1] + '</a></td><td><a href="#">' + datapoints[i+2] + '</a></td></tr>';
+            var img_link = datapoints[i+5].replace('/file/d/','/uc?export=view&id=').replace('/open?id=', '/uc?export=view&id=')
             var input = '\
             <div class="team-card col-sm-3 card-container">\
-                    <img class="cirlce-image center" src='+datapoints[i+5].replace('/file/d/','/uc?export=view&id=').replace('/open?id=', '/uc?export=view&id=')+'alt='+datapoints[i+5]+'>\
+                    <img class="cirlce-image center" src='+img_link+'alt='+img_link+'>\
                     <p><b>'+datapoints[i+1]+'</b>\
                         <br>\
                         '+datapoints[i+2]+'\
