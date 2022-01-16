b3c4c9de4a0789ffd88b2e528399ef933cd7449d
Ishaan Gupta <ishaangupta@Ishaans-MacBook-Pro-2.local>
2022-01-16 15:19:46 -0800

    Revert "Update table.js"
    This reverts commit d69914b6bf4ca59597aa10b1398513d882bfc964.

--- bootcamp/js/table.js
+++ bootcamp/js/table.js
@@ -1,24 +1,24 @@
 function tableUpdate(link, target) {
     $.ajax(link).done(function(result){
-        var datapoints = result.split(/\n/).join("\t").split("\t");
+        var datapoints = result.split(/\n/).join(",").split(",");
         for(let i = 6; i<datapoints.length; i+=6){
             var lesson = '<tr><th scope="row">' + datapoints[i] + '</th>';
-            var resource = datapoints[i+1].startswith("http") ? '<td><a href="' + datapoints[i+1] +'"> Lesson Link' + '</a></td>': '<td>TBD</td>';
-//             if( ! datapoints[i+1].startswith("http")){
-//                 resource = '<td>TBD</td>'
-//             }
+            var resource = '<td><a href="' + datapoints[i+1] +'"> Lesson Link' + '</a></td>';
+            if(datapoints[i+1] == "#"){
+                resource = '<td>TBD</td>'
+            }
             var date = '<td>' + datapoints[i+2] + '</td>';
             var week = '<td>' + datapoints[i+3] + '</td>';
-            var recording = datapoints[i+4].startswith("http") ? '<td><a href="'+ datapoints[i+4]+'">Recording Link</a></td>' : '<td>Sorry, No Recording</td>';
-//             if(! datapoints[i+4].startswith("http")){
-//                 var recording = '<td>Sorry, No Recording</td>';
-//             }
-            var folder = datapoints[i+5].startswith("http") ? '<td><a href="'+datapoints[i+5]+'"> Folder Link</a></td></tr>' : '<td>TBD</td>';
-//             if(! datapoints[i+5].startswith("http")){
-//                 folder = '<td>TBD</td>';
-//             } else{
-//                 folder = '<td><a href="'+datapoints[i+5]+'"> Folder Link</a></td></tr>';
-//             }
+            var recording = '<td><a href="'+ datapoints[i+4]+'">Recording Link</a></td>';
+            if(datapoints[i+4] == "#"){
+                var recording = '<td>Sorry, No Recording</td>';
+            }
+            var folder;
+            if(datapoints[i+5] == "#"){
+                folder = '<td>TBD</td>';
+            } else{
+                folder = '<td><a href="'+datapoints[i+5]+'"> Folder Link</a></td></tr>';
+            }
             //var input = '<tr><th scope="row">' + datapoints[i] + '</th><td><a href="' + datapoints[i+1] +'"> Lesson Link' + '</a></td><td>' + datapoints[i+2] + '</td><td>' + datapoints[i+3] + '</td><td><a href="'+ datapoints[i+4]+'">Recording Link</a></td>'+'<td><a href="'+datapoints[i+5]+'"> Folder Link</a></td></tr>';
             var input = lesson + resource + date + week + recording + folder;
             $(target).append(input);
@@ -26,5 +26,5 @@ function tableUpdate(link, target) {
     });
 }
 
-tableUpdate("https://docs.google.com/spreadsheets/d/e/2PACX-1vQwzVDavSQaIw281Xwld-QiHyone_9L_dl5HDP9n8FSKWrRd3Bxna8IHoA0exxurQFvCiUG0611hFTc/pub?output=tsv","#fall-target");
-tableUpdate("https://docs.google.com/spreadsheets/d/e/2PACX-1vRSPISLGaN2ErEqLcmtuxmbh41oXliuC3c2sSNG-MsLfhsw5zEClAd54oImdzLjbIN28mOulgWdHXHJ/pub?output=tsv", "#winter-target");
+tableUpdate("https://docs.google.com/spreadsheets/d/e/2PACX-1vQwzVDavSQaIw281Xwld-QiHyone_9L_dl5HDP9n8FSKWrRd3Bxna8IHoA0exxurQFvCiUG0611hFTc/pub?gid=0&single=true&output=csv","#fall-target");
+tableUpdate("https://docs.google.com/spreadsheets/d/e/2PACX-1vQuTlHsHOHswd1Giw9kAQa97z1aFcslzAmpLwdf8IugidPgxXaTCIXgNVtCguB2NT6b7fZywV9Z9wKh/pub?gid=0&single=true&output=csv", "#winter-target");
