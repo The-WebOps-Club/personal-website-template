74ba3af3c8e41d5e5c86cac060e9f28dc069812d
realvudi <stivengolzari@gmail.com>
2023-08-10 12:31:25 -0700

    Updated Photos of team

--- team/js/fill.js
+++ team/js/fill.js
@@ -1,23 +1,30 @@
 function dataAlert() {
-    $.ajax("https://docs.google.com/spreadsheets/d/e/2PACX-1vQnzIdm2KfxMnqb_s9VaTGPJSejGw8vb8_ygRC7UcQyTTWpAVz5FiHNH_WI_tDsTOeDwo0rf3aUrWv1/pub?output=tsv").done(function(result){
+    $.ajax("https://docs.google.com/spreadsheets/d/e/2PACX-1vTQrpOKozWQTb1EdU2CfCur2g-va_F8TOx0Fz0aFInP0-Eoxh7xtD7PIyxu7QrIdBCPHVfMeERFrUGD/pub?gid=0&single=true&output=tsv").done(function(result){
         var datapoints = result.split(/\n/).join("\t").split("\t");
-        for(let i = 6; i<datapoints.length; i+=6){
-            // var input = '<tr><th scope="row">' + datapoints[i] + '</th><td><a href="#">' +datapoints[i+1] + '</a></td><td><a href="#">' + datapoints[i+2] + '</a></td></tr>';
-            var img_link = datapoints[i+5];
-            img_link = img_link.replace('/file/d/','/uc?export=view&id=').replace('/open?id=', '/uc?export=view&id=');
+        // console.log(datapoints);
+        for (let i = 0; i < datapoints.length; i += 7){
+            const firstName = datapoints[i],
+                lastName = datapoints[i + 1],
+                position = datapoints[i + 2],
+                graduationDate = datapoints[i + 3],
+                major = datapoints[i + 4],
+                email = datapoints[i + 5];
+            let image = datapoints[i + 6];
+
+            
             var input = '\
             <div class="team-card col-sm-3 card-container">\
-                    <img class="cirlce-image center" src="'+img_link+'"alt='+img_link+'>\
-                    <p><b>'+datapoints[i+1]+'</b>\
+                    <img class="cirlce-image center" src="Board/'+image+'"alt=Board/'+image+'>\
+                    <p><b>'+firstName + ' ' + lastName + '</b>\
                         <br>\
-                        '+datapoints[i+2]+'\
+                        '+position+'\
                         <br />\
-                        <!-- <u>jim095@ucsd.edu</u> -->\
-                        <a class="email" href="mailto: '+datapoints[i+3]+'">'+datapoints[i+3]+'</a>\
-                    </p>\
-                    <div class="overlay">\
+                        \
+                        </p>\
+                        <div class="overlay">\
                         <div class="text">\
-                        '+datapoints[i+4]+'\
+                        <p>'+major +' ' + graduationDate +'</p>\
+                        <a  href="mailto: '+email+'">'+email+'</a>\
                         </div>\
                     </div>\
                 </div>\
